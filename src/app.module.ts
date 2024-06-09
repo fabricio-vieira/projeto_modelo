a7525cdf2133a8da7388496fb5c8abab9395f67f
fabricio-vieira <fabricio_raldi@outlook.pt>
2024-06-09 12:36:23 -0400

    adjust template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common'
 import { HttpModule } from './http/http.module'
-import { DatabaseModule } from './database/database.module'
-import { PrismaService } from './database/prisma/prisma.service';
-import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
+// import { DatabaseModule } from './database/database.module'
+
+
 
 @Module({
-  imports: [HttpModule, DatabaseModule, UserModule],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [HttpModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { ProjectController } from './project/project.controller'
+import { ProjectService } from './project/project.service'
 
 @Module({
-    imports: [ConfigModule.forRoot()]
+    imports: [ConfigModule.forRoot()],
+    controllers: [ProjectController],
+    providers: [ProjectService]
 })
 export class HttpModule { }
